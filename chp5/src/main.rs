965f20e3981564363e14e3ada44033974df2695c
RokiLord <youht7290@gmail.com>
2022-07-09 04:19:40 +0900

    commit chp5

--- chp5/src/main.rs
+++ chp5/src/main.rs
@@ -1,3 +1,38 @@
-fn main() {
-    println!("Hello, world!");
+#[derive(Debug, Clone)]
+#[allow(unused_variables)]
+
+struct Character {
+    name : String,
+    health : i32,
+    alive : bool,
+    
 }
+fn main(){
+    let mut terry = Character{
+        name : String::from("terry"),
+        health: 100,
+        alive : true,
+    };
+
+    let mut john = Character{
+        name : String::from("john"),
+        health: 100,
+        alive : true,
+    };
+
+    
+    println!("{}", terry.getName());
+    println!("{}", john.getName());
+
+}
+
+
+
+
+impl Character{
+    fn getName(&self) -> &str{
+        &self.name
+    }
+
+
+}   
